1ceae938f6db60bb1c2ec13e2c9c3c20eae328ee
Jack Heuberger <jackheuberger@wustl.edu>
2023-03-28 11:34:44 -0500

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/lsp.lua

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -1,44 +1,39 @@
--- stylua: ignore
 return {
-    -- tresitter required plugins
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = {
-            ensure_installed = {
-                "bash",
-                "help",
-                "html",
-                "javascript",
-                "json",
-                "lua",
-                "markdown",
-                "markdown_inline",
-                "python",
-                "query",
-                "regex",
-                "tsx",
-                "typescript",
-                "vim",
-                "yaml",
-            },
-        },
+  -- tresitter required plugins
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "help",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
     },
+  },
 
-    {
-        "neovim/nvim-lspconfig",
-        ---@class PluginLspOpts
-        opts = {
-            ---@type lspconfig.options
-            servers = {
-                -- pyright will be automatically installed with mason and loaded with lspconfig
-                pyright = {},
-                clangd = {},
-            },
-            setup = {},
-        },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      setup = {
+        clangd = function(_, opts)
+          opts.capabilities.offsetEncoding = { "utf-16" }
+        end,
+      },
     },
-
-    {
-        "github/copilot.vim",
-    },
+  },
+  {
+    "github/copilot.vim",
+  },
 }
